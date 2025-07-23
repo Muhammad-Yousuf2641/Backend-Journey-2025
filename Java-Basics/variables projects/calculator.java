e2370323ac5ab4dc556ab3cc5a25f57087bde4fb
Muhammad_Yousuf2641 <m.yousufbio2001@gmail.com>
2025-07-23 14:24:18 +0500

    calulator project

---
+++ Java-Basics/variables projects/calculator.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+     //declare variables
+     int num1;
+     int num2;
+     String oper;
+     
+    // create scanner object 
+    System.out.println(" Enter Numbers :");
+    Scanner obj = new Scanner(System.in);
+    num1=obj.nextInt();
+    num2=obj.nextInt();
+    obj.nextLine();
+    
+    // assiging values to the variables
+    System.out.print(" Select operator: ");
+    oper=obj.nextLine();
+
+    //operations 
+        int sum=num1+num2;
+        int sub=num1-num2;
+        int mul=num1*num2;
+        int div=num1/num2;
+
+    //  conditions 
+    if(oper.equals("+")){
+        System.out.println(sum);
+    }
+    else if(oper.equals("-")){
+        System.out.println(sub);
+    }
+    else if(oper.equals("*")){
+        System.out.println(mul);
+    }
+    else if(oper.equals("/")){
+        System.out.println(div);
+    }
+    else{
+        System.out.println("invalid operator");
+    }
+
+obj.close();
+    }
+}
