ea09e11c42264422060380fc36325a4daeb25ead
Muhammad_Yousuf2641 <m.yousufbio2001@gmail.com>
2025-07-23 12:34:56 +0500

    first commit

---
+++ Java-Basics/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String args[]) {
+
+        //  Declare a variables 
+        final int a;
+        int b;
+        int c;
+        // assign the values to the varables
+
+        a=10;
+        b=20;
+        c=a+b;
+        // reassging the values 
+        
+        // a=15;
+
+        // it can't reassgin value because it contained 'a' final keyword which can't modify the value of variable 
+
+        b=25;
+        c=a+b;
+
+        // print variables 
+        System.out.println(c);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Backend-Journey-2025
+Documenting my 60-day challenge to become a backend developer 💻 — from Java fundamentals to real-world REST APIs, MySQL integration, cloud deployment, and DSA practice. Building projects, sharing progress, and preparing for real job opportunities! 🚀
+</br>
+#codingjourney #backendmasterplan
